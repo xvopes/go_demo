1e0f5843cf8678cdab19f7f6c026ffeb48d6edc9
xvopes <xvopes@gmail.com>
2020-03-20 00:52:26 +0800

    "template"

--- gin_demo/main.go
+++ gin_demo/main.go
@@ -22,21 +22,51 @@
 
 package main
 
+//
+//import (
+//	"github.com/gin-gonic/gin"
+//)
+//
+//func sayHello(ctx *gin.Context) {
+//	ctx.JSON(200, gin.H{
+//		"message": "Hello,golang",
+//	})
+//}
+//func main() {
+//	r := gin.Default() //返回默认的路由引擎
+//
+//	r.GET("/hello", sayHello)
+//
+//	r.GET("/book")
+//
+//	r.Run(":9091")
+//}
+
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
-func sayHello(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "Hello,golang",
-	})
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("Parse template failed,err%v", err)
+		return
+	}
+
+	name := "小王子"
+	err = t.Execute(w, name)
+	if err != nil {
+		fmt.Println("render template failed,err:%v", err)
+		return
+	}
 }
 func main() {
-	r := gin.Default() //返回默认的路由引擎
-
-	r.GET("/hello", sayHello)
-
-	r.GET("/book")
-
-	r.Run(":9091")
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9100", nil)
+	if err != nil {
+		fmt.Println("Http server start failed, err:%v", err)
+		return
+	}
 }
