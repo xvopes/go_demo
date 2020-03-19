238a68f9de03f86651083f0b2ebc7e949b264bbc
xvopes <xvopes@gmail.com>
2020-03-20 01:10:02 +0800

    "template"

---
+++ lesson05/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	Name   string
+	Gender string
+	Age    int
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	//定义模版
+	//解析模版
+	t, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("Parse template failed,err%v", err)
+		return
+	}
+	//渲染模版
+	u1 := User{
+		Name:   "小王子",
+		Gender: "男",
+		Age:    23,
+	}
+	t.Execute(w, u1)
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9100", nil)
+	if err != nil {
+		fmt.Println("Http server start failed, err:%v", err)
+		return
+	}
+}
